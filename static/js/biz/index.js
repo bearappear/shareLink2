e1d7c015af36108c337f9d6089f664c19b797f4a
bearAppear <bearappear@yahoo.com>
2023-05-25 20:15:06 +0800

    Changes1

--- static/js/biz/index.js
+++ static/js/biz/index.js
@@ -2,7 +2,7 @@ $('.B_contact').click(function() {
     let link = $(this).data('link');
     window.open(link)
 })
-let zaloNumber1 = '+84338228680';
-let zaloNumber2 = '+84857534889';
-let zaloNumber3 = '+84914025948';
-let zaloNumber4 = '+84923091787';
+let zaloNumber1 = '+84857534889';
+let zaloNumber2 = '+84914025948';
+let zaloNumber3 = '+84923091787';
+let zaloNumber4 = zaloNumber1;
