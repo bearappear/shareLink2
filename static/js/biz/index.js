fedd4a5e8175b5e0b1786ae464b3a4d5ea65d1fa
bearAppear <bearappear@yahoo.com>
2023-05-25 20:48:31 +0800

    Changes2

--- static/js/biz/index.js
+++ static/js/biz/index.js
@@ -2,7 +2,13 @@ $('.B_contact').click(function() {
     let link = $(this).data('link');
     window.open(link)
 })
-let zaloNumber1 = '+84857534889';
-let zaloNumber2 = '+84914025948';
-let zaloNumber3 = '+84923091787';
-let zaloNumber4 = zaloNumber1;
+let number1 = '+84857534889';
+let number2 = '+84914025948';
+let number3 = '+84923091787';
+let number4 = number1;
+let wxname = 'Tham gia đội nhóm của chúng tôi và ';
+function talk(number) {
+    number = number || number1;
+    // console.log(number);
+    window.location.href = "https://zalo.me/" + number;
+}
